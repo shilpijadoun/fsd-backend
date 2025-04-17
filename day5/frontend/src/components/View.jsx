31df4559acef5008bb551c851e8cb71822bfc5bc
shilpijadoun <rajputshilpi141@gmail.com>
2025-04-17 15:12:09 +0530

    updated day5

--- day5/frontend/src/components/View.jsx
+++ day5/frontend/src/components/View.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const View = () => {
     const [users,setUsers]=useState([])
     const handleview=async () => {
-        const res=await axios.get('http://localhost:9000/users')
+        const res=await axios.get('https://fsd-backend-024m.onrender.com/users')
         setUsers(res.data)
     }
     useEffect(()=>{
