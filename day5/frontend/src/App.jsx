d7a6066e3bc56df63096f37241f6629e7a21e081
shilpijadoun <rajputshilpi141@gmail.com>
2025-04-17 14:18:32 +0530

    day5

---
+++ day5/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Register from './components/Register'
+
+const App = () => {
+  return (
+    <div>
+      <h1>User Registration System</h1>
+      <Register/>
+    </div>
+  )
+}
+
+export default App
